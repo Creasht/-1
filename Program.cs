66371352ea518c725df13e164a9b649ed2fc5556
Crasht <Creasht@gmail.com>
2023-02-01 12:19:50 +0200

    решение задачи 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    System.Console.WriteLine("Наибольшее число " + numberA);
+}
+else 
+{
+    System.Console.WriteLine("Наибольшее число " + numberB);
+}
